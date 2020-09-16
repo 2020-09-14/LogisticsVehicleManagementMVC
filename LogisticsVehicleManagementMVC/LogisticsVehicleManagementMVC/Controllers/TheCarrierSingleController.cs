eb87bd94bc549a484fe32cf29c0fbcc84c6661ec
ttf0512 <1799441665@qq.com>
2020-09-16 15:23:22 +0800

    更新 Layui

---
+++ LogisticsVehicleManagementMVC/LogisticsVehicleManagementMVC/Controllers/TheCarrierSingleController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogisticsVehicleManagementMVC.Controllers
+{
+    public class TheCarrierSingleController : Controller
+    {
+        public IActionResult GettheCarrierSingles()
+        {
+            return View();
+        }
+    }
+}
