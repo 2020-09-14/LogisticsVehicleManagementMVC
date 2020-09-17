e6d3641f5cc81d7408364dbcb9d0e90694e2f2a1
张春 <2934024192@qq.com>
2020-09-17 08:59:13 +0800

    车辆更新

---
+++ LogisticsVehicleManagementMVC/LogisticsVehicleManagementMVC/Controllers/CarShowController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogisticsVehicleManagementMVC.Controllers
+{
+    public class CarShowController : Controller
+    {
+        
+        public IActionResult Show()
+        {
+            return View();
+        }
+        public IActionResult CarAdd()
+        {
+            return View();
+        }
+    }
+}
