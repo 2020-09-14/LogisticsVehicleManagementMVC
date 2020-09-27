4c3f0b3aeca9255474eed7019154718d5275b7f7
HP <HP@DESKTOP-HNEDNFK>
2020-09-27 10:07:27 +0800

    666

---
+++ LogisticsVehicleManagementMVC/LogisticsVehicleManagementMVC/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogisticsVehicleManagementMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        //登录
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Amend()
+        {
+            return View();
+        }
+
+        public IActionResult It()
+        {
+            return View();
+        }
+    }
+}
