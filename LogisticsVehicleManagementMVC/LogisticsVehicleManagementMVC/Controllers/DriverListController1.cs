7453ca5c3241dbd5bc3ff6cd145fe161de090921
文杰 申 <1066321371@qq.com>
2020-09-27 11:03:23 +0800

    完成

---
+++ LogisticsVehicleManagementMVC/LogisticsVehicleManagementMVC/Controllers/DriverListController1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogisticsVehicleManagementMVC.Controllers
+{
+    public class DriverListController1 : Controller
+    {
+        //显示驾驶员
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Show()
+        {
+            return View();
+        }
+        //添加驾驶员
+        public IActionResult Add()
+        {
+            return View();
+        }
+        //更新驾驶员
+        public IActionResult Upt(int Did) 
+        {
+            ViewBag.Id = Did;
+            return View();
+        }
+    }
+}
---
+++ LogisticsVehicleManagementMVC/LogisticsVehicleManagementMVC/Controllers/Layui.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogisticsVehicleManagementMVC.Controllers
+{
+    public class Layui : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
